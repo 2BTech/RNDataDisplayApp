4949cd0f40890f08677784cb269d92173e765134
kopalatommy <kopalatommy@gmail.com>
2023-08-22 11:17:30 -0600

    Adding new splash screen

--- ios/TwoBConnect/AppDelegate.mm
+++ ios/TwoBConnect/AppDelegate.mm
@@ -11,7 +11,9 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  BOOL result = [super application:application didFinishLaunchingWithOptions:launchOptions];
+  self.window.rootViewController.view.backgroundColor = [UIColor colorWithRed:0.50588 green:0.83137 blue:0.94118 alpha:1.00];
+  return result;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
